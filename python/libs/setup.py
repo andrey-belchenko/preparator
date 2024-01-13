e8dc83493fd1c3b93a514a0d097b3b9fc1fefedc
Andrey Belchenko <abelchenko@conteq.ru>
2024-01-13 23:50:35 +0300

    prep python part

--- python/libs/setup.py
+++ python/libs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='df_prep',  
-    version='0.1.6',
+    version='0.1.7',
     author='DF',
     author_email='your_email@example.com',
     packages=find_packages(),  
