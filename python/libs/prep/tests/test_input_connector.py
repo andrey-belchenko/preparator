988382edeb623ac6ea77d80bb5ad9abfa21632ec
Andrey Belchenko <abelchenko@conteq.ru>
2024-01-13 18:16:53 +0300

    prep py classes draft

---
+++ python/libs/prep/tests/test_input_connector.py
@@ -0,0 +1,11 @@
+import unittest
+from connectors import InputConnector
+
+class TestInputConnector(unittest.TestCase):
+    def test_greet(self):
+        obj = InputConnector("Bard")
+        greeting = obj.greet()
+        self.assertEqual(greeting, "Hello, my name is Bard")
+
+if __name__ == "__main__":
+    unittest.main()
