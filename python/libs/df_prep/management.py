23400ba177a2c919b0158562240c30509adf38ca
Andrey Belchenko <abelchenko@conteq.ru>
2024-01-27 02:57:20 +0300

    publish

--- python/libs/df_prep/management.py
+++ python/libs/df_prep/management.py
@@ -12,18 +12,42 @@ def publish_project(
     main_func_name: str,
     include: list[str] = None,
 ):
-    tmp_path = _copy_files_to_temp_dir( root_path, include)
-    print(tmp_path)
-    _run_in_temp_dir(tmp_path, main_file_path, main_func_name)
+    build_project(root_path, main_file_path, main_func_name, include)
+
+
+def build_project(
+    root_path: str,
+    main_file_path: str,
+    main_func_name: str,
+    include: list[str] = None,
+):
+    tmp_path = _copy_files_to_temp_dir(root_path, include)
+    project = _run_in_temp_dir(tmp_path, main_file_path, main_func_name)
+    archive_path = _make_archive(tmp_path, project.name)
+
+
+def _make_archive(tmp_path, project_name):
+    print("Make archive: start")
+    archive_path = os.path.join(
+        os.path.dirname(tmp_path), "df_prep_projects", f"{project_name}"
+    )
+    archive_path_ext = archive_path + ".zip"
+    if os.path.isfile(archive_path):
+        os.remove(archive_path)
+    shutil.make_archive(archive_path, "zip", tmp_path)
+    shutil.rmtree(tmp_path)
+    print("Make archive: success")
+    print(f"- temp dir path: {archive_path_ext}")
+    return archive_path_ext
 
 
 def _copy_files_to_temp_dir(root_path, include: list[str] = None):
+    print("Collect source files: start")
     tmp_path = os.path.join(root_path, "build", "df_prep_temp")
 
     if os.path.isdir(tmp_path):
         shutil.rmtree(tmp_path)
     os.makedirs(tmp_path)
-    
 
     if include == None:
         include = os.listdir(root_path)
@@ -40,11 +64,13 @@ def _copy_files_to_temp_dir(root_path, include: list[str] = None):
         else:
             os.makedirs(os.path.dirname(trg_path))
             shutil.copy(src_path, trg_path)
-
+    print("Collect source files: success")
+    print(f"- temp dir path: {tmp_path}")
     return tmp_path
 
 
 def _run_in_temp_dir(tmp_path, main_file_path, main_func_name):
+    print("Build project: start")
     sys.path.append(tmp_path)
     main_file_path_full_path = os.path.join(tmp_path, main_file_path)
     spec = spec_from_file_location("module", main_file_path_full_path)
@@ -56,10 +82,11 @@ def _run_in_temp_dir(tmp_path, main_file_path, main_func_name):
         raise Exception(
             f"Main function '{main_func_name}' should return instance of 'df_prep.Project' class"
         )
-    print("Project build: success")
-    print(f"project: {project.name}")
+    print("Build project: success")
+    print(f"- project: {project.name}")
     for module_name in project.modules:
         module = project.modules[module_name]
-        print(f"- module: {module.name}")
+        print(f"- - module: {module.name}")
         for proc_name in module.processors:
-            print(f"- - processor: {proc_name}")
+            print(f"- - - processor: {proc_name}")
+    return project
