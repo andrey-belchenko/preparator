6e26b8b2f3dbd74ffacfbae54cd52613daeec138
Andrey Belchenko <abelchenko@conteq.ru>
2024-01-29 01:45:07 +0300

    api

--- python/libs/api/app.py
+++ python/libs/api/app.py
@@ -1,10 +1,9 @@
-from datetime import datetime
+
 from fastapi import FastAPI, HTTPException, Path, Query, Body
-from pydantic import BaseModel
 from pymongo import MongoClient
 from pyparsing import Any
 
-from api.models import Project
+from api.models import ProjectInfo, ModuleInfo, ProcessorInfo
 
 app = FastAPI(port=8000)
 mongo = MongoClient(f"mongodb://root:eximer@mongodb.mrsk.oastu.lan:27017")
@@ -12,18 +11,48 @@ mongo = MongoClient(f"mongodb://root:eximer@mongodb.mrsk.oastu.lan:27017")
 collection_prefix = "sys_python_"
 project_coll_name = f"{collection_prefix}project"
 module_coll_name = f"{collection_prefix}module"
-
+processor_coll_name = f"{collection_prefix}processor"
 
 @app.get("/projects")
-async def get_projects(db_name: str = Query(...)) -> list[Project]:
+async def get_projects(db_name: str = Query(...)) -> list[ProjectInfo]:
     return mongo[db_name][project_coll_name].find({})
 
 
-@app.get("/projects/{project_name}")
+@app.get("/projects/{name}")
 async def get_project(
-    project_name: str = Path(...), db_name: str = Query(...)
-) -> Project:
-    item = mongo[db_name][project_coll_name].find_one({"name": project_name})
+    name: str = Path(...), db_name: str = Query(...)
+) -> ProjectInfo:
+    item = mongo[db_name][project_coll_name].find_one({"name": name})
+    if item is None:
+        raise HTTPException(status_code=404)
+    return item
+
+@app.get("/modules")
+async def get_modules(db_name: str = Query(...)) -> list[ModuleInfo]:
+    return mongo[db_name][module_coll_name].find({})
+
+
+@app.get("/modules/{name}")
+async def get_module(
+    name: str = Path(...), db_name: str = Query(...)
+) -> ModuleInfo:
+    item = mongo[db_name][module_coll_name].find_one({"name": name})
+    if item is None:
+        raise HTTPException(status_code=404)
+    return item
+
+
+
+@app.get("/processors")
+async def get_processors(db_name: str = Query(...)) -> list[ProcessorInfo]:
+    return [mongo[db_name][processor_coll_name].find_one({})]
+
+
+@app.get("/processors/{name}")
+async def get_processor(
+    name: str = Path(...), db_name: str = Query(...)
+) -> ProcessorInfo:
+    item = mongo[db_name][processor_coll_name].find_one({"name": name})
     if item is None:
         raise HTTPException(status_code=404)
     return item
--- python/libs/api/models.py
+++ python/libs/api/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from typing import Any, Optional
 from pydantic import BaseModel
 
 
-class Project(BaseModel):
+class ProjectInfo(BaseModel):
     class VersionInfo(BaseModel):
         repository: str
         branch: str
@@ -10,10 +11,34 @@ class Project(BaseModel):
         is_dirty: bool
 
     name: str
-    changed_at: datetime 
+    changed_at: datetime
     deployment_id: str
     main_file_path: str
     main_func_name: str
     published_by: str
-    version_info: VersionInfo
+    version_info: Optional[VersionInfo] = None
     deployment_id: str
+
+
+class ModuleInfo(BaseModel):
+    name: str
+    project: str
+    defined_in_file: str
+
+
+class ProcessorInfo(BaseModel):
+    class PortInfo(BaseModel):
+        name: str
+        read_only: bool
+        title: Optional[str] = None
+        description: Optional[str] = None
+        schema: Optional[Any] = None
+
+    name: str
+    project: str
+    module: str
+    defined_in_file: str
+    title: Optional[str] = None
+    description: Optional[str] = None
+    inputs: list[PortInfo]
+    outputs: list[PortInfo]
