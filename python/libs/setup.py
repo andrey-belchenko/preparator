590fe98e400f443d4bcd6f0d944f1cfaed16a0d8
Andrey Belchenko <abelchenko@conteq.ru>
2024-02-01 06:17:15 +0300

    api

--- python/libs/setup.py
+++ python/libs/setup.py
@@ -3,9 +3,12 @@ from setuptools import setup
 
 setup(
     name="dpt",
-    version="0.1.8",
+    version="0.2.0",
     author="DF",
     author_email="your_email@example.com",
     packages=["dpt"],
-    install_requires=[],
+    install_requires=[
+        "GitPython>=3.1.41",
+        "pymongo>=4.6.1",
+    ],
 )
