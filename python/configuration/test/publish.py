14e1c678425a336524d6b5bd76d0a36721654b05
Andrey Belchenko <abelchenko@conteq.ru>
2024-02-01 07:17:13 +0300

    -

--- python/configuration/test/publish.py
+++ python/configuration/test/publish.py
@@ -7,9 +7,9 @@ management.set_connection("mongodb://root:eximer@mongodb.mrsk.oastu.lan:27017")
 management.set_workspace("bav_test4")
 
 management.deploy_project(
-    root_path=r"C:\Repos\mygithub\preparator\python\libs",
-    include=["run/processors"],
-    main_file_path=r"run/processors/main.py",
+    root_path=r"C:\Repos\mygithub\preparator\python\configuration",
+    include=["processors"],
+    main_file_path=r"processors/main.py",
     main_func_name="create_project",
 )
 
--- python/libs/dpt/__init__.py
+++ python/libs/dpt/__init__.py
@@ -1,11 +1,3 @@
-# from .classes.processor_old import Processor
-# from .classes.processor_old import ProcessorParams
-# from .classes.processor_old import ProcessorParam
-# from .classes.processor_old import Secret
-# from .classes.storage import DbConnection
-# from .classes.storage import Database
-# from .classes.storage import DbReader
-# from .classes.storage import DbWriter
 from .processor import Project
 from .processor import Task
 from .processor import Module
