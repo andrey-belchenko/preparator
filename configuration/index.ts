8b44ad930c9855f00f6890ef3cc4456481cac731
Andrey Belchenko <abelchenko@conteq.ru>
2024-02-05 02:38:53 +0300

    configuration

---
+++ configuration/index.ts
@@ -0,0 +1,10 @@
+module attempt2 {
+    type DataSet = {};
+  
+    export function input_ya_disk_excel(inputs?: { params: DataSet }): string {
+      return "yep";
+    }
+  }
+  
+  console.log(attempt2.input_ya_disk_excel());
+  
