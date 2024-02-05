b7efde81b93abf21d827c4ec37880d2c0759613d
Andrey Belchenko <abelchenko@conteq.ru>
2024-02-05 10:44:14 +0300

    builder

--- configuration/index.ts
+++ configuration/index.ts
@@ -1,10 +1,36 @@
 module attempt2 {
-    type DataSet = {};
-  
-    export function input_ya_disk_excel(inputs?: { params: DataSet }): string {
-      return "yep";
-    }
+  type DataSet = {};
+
+  export function input_ya_disk_excel(pars: DataSet) {
+    return { def: {} };
   }
+
+  export function clear_rs_data(def: DataSet) {
+    return { def: {} };
+  }
+
+  export function load_rs(pars: DataSet) {
+    let input = input_ya_disk_excel(pars)
+    let clear = clear_rs_data(input)
+    return clear;
+  }
+
+  const funcs:any = {}
   
-  console.log(attempt2.input_ya_disk_excel());
-  
+  export function steps(pars: DataSet) {
+    let input = funcs["input_ya_disk_excel"].call(pars)
+    let clear = funcs["clear_rs_data"].call(input)
+    return clear;
+  }
+  //   export function input_ya_disk_excel({
+  //     input1,
+  //     input2,
+  //   }: {
+  //     input1: DataSet;
+  //     input2: DataSet;
+  //   }): { output1: DataSet } {
+  //     return { output1: {} };
+  //   }
+}
+
+let a = attempt2.input_ya_disk_excel({ input1: {}, input2: {} });
