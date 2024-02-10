06fb8f0c42899ba9cd826bf5c734b779b80e760a
Andrey Belchenko <abelchenko@conteq.ru>
2024-02-11 02:59:15 +0300

    clear usage

--- python/system/usage/processors/utils/excel.py
+++
@@ -1,9 +0,0 @@
-import io
-import pandas as pd
-from run.processors.utils import data_frame
-
-
-def read_excel(data: io.BytesIO) -> pd.DataFrame:
-    df = pd.read_excel(data)
-    df = df.replace({pd.NaT: None})
-    return df
